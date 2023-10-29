270322debf14f9ca6e12393c0e7f2db77164552f
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-10-29 07:00:08 +0200

    Fix basename

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
-import { Loader } from "./components/Loader";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { Loader } from './components/Loader';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<Loader/>} persistor={persistor}>
-        <BrowserRouter>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
